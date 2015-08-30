46f5caf1f248a6415b4536f713c2f1f540c22e48
elsehow <yes@cosmopol.is>
2015-08-30 10:04:46 -0700

    ..

--- backup-this.sh
+++
@@ -1,1 +0,0 @@
-duplicity --encrypt-key C3D82B5E /home/ffff scp://nick@ischool.berkeley.edu/backup/elsehow
--- restore-home.sh
+++
@@ -1,7 +0,0 @@
-!/bin/sh
-
-test -x $(which duplicity) || exit 0
-. .passphrase
-
-export PASSPHRASE
-$(which duplicity) --encrypt-key C3D82B5E scp://nick@ischool.berkeley.edu/backup/elsehow/ /home/ffff/restore/
