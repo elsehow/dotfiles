eb19e6b303ed76a193ff7c8be4fefb03c78c90cd
elsehow <yes@cosmopol.is>
2015-09-24 17:03:44 -0700

    updated for install on ubuntu

--- bootstrap-installs.sh
+++ bootstrap-installs.sh
@@ -1,14 +1,6 @@
-
-
-# nodejs + npm
-sudo add-apt-repository ppa:chris-lea/node.js  
-sudo apt-get update  
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install git-core zsh vim curl make # python2.7 git-all pkg-config libncurses5-dev libssl-dev libnss3-dev libexpat-dev
 
 # install zsh and set that as shell?
 # this won't take effect until after restart
-sudo apt-get install zsh
-sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
