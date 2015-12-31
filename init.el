47c742d0fca7908b2ae6b0e67f5184e1001e573e
elsehow <yes@cosmopol.is>
2015-12-31 10:56:20 -0500

    better mouse support

--- init.el
+++ init.el
@@ -23,10 +23,19 @@
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 ; enable mouse
-(require 'mouse)
-(xterm-mouse-mode t)
-(defun track-mouse (e))
-(setq mouse-sel-mode t)
+;; Enable mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
 
 ;; Enable Evil mode as defuault
 (setq evil-want-C-i-jump nil)
