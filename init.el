52deaa25b4c17342e3800fdd68264f6b9313c6e8
elsehow <yes@cosmopol.is>
2016-01-24 09:52:07 -0800

    updated emacs config to include my agenda files

--- init.el
+++ init.el
@@ -113,7 +113,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Notes/winterrr.org")))
+ '(org-agenda-files (quote ("~/Notes/fun.org" "~/Notes/spring.org")))
  '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
