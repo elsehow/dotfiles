9e27d8bee409864682f2dba6a87ee9bd1be71769
elsehow <yes@cosmopol.is>
2016-01-26 17:09:10 -0800

    updated emacs.d/init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -113,7 +113,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Notes/fun.org" "~/Notes/spring.org")))
+ '(org-agenda-files (quote ("~/Notes/fun.org"
+                            "~/Notes/spring.org"
+                            "~/Projects/dissertation/prospectus.org"
+                            )))
  '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
